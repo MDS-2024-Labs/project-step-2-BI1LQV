31fd6331e1ee272ceb418be51029e383e336e428
bi1lqv <bi1lqv.y@gmail.com>
2024-12-09 21:46:59 -0800

    test: more meaningful

--- test/test_Watch.py
+++ test/test_Watch.py
@@ -13,21 +13,23 @@ class TestWatch(unittest.TestCase):
 
     def setUp(self):
         self.R1 = Reactive({"first_name": "John", "last_name": "Doe", "age": 20})
-        self._executed_1 = False
+        # this flag is used to check if the watch effect is triggered
+        self.effect1_exeuted = False
         def watchLambda1():
             self.R1.name
             self.R1.age
-            self._executed_1 = True
+            # effect is triggered and flag to be true
+            self.effect1_exeuted = True
         self.W1 = Watch(watchLambda1)
-        self._executed_1 = False
+        self.effect1_exeuted = False
 
         self.full_name = Computed(lambda: self.R1.first_name + " " + self.R1.last_name)
-        self._executed_2 = False
+        self.effect2_exeuted = False
         def watchLambda2():
             self.full_name.value
-            self._executed_2 = True
+            self.effect2_exeuted = True
         self.W2 = Watch(watchLambda2)
-        self._executed_2 = False
+        self.effect2_exeuted = False
 
         
 
@@ -35,44 +37,47 @@ class TestWatch(unittest.TestCase):
         self.R1 = None
         self.W1.stop()
         self.W1 = None
-        self._executed_1 = False
+        self.effect1_exeuted = False
 
         self.full_name = None
         self.W2.stop()
         self.W2 = None
-        self._executed_2 = False
+        self.effect2_exeuted = False
     
     def test_name(self):
         self.R1.first_name = "Mike"
         self.R1.last_name = "Smith"
         # watch effect should be triggered for reactive and computed
-        self.assertTrue(self._executed_1)
-        self.assertTrue(self._executed_2)
+        # and flag is true means the watch effect is triggered
+        self.assertTrue(self.effect1_exeuted)
+        self.assertTrue(self.effect2_exeuted)
 
-        self._executed_1 = False
-        self._executed_2 = False
+        self.effect1_exeuted = False
+        self.effect2_exeuted = False
         self.W1.stop()
         self.W2.stop()
         self.R1.first_name = "Jane"
         self.R1.last_name = "Doe"
         # watch stoped, watch effect should not be triggered
-        self.assertFalse(self._executed_1)
-        self.assertFalse(self._executed_2)
+        # and flag is false means the watch effect is not triggered
+        self.assertFalse(self.effect1_exeuted)
+        self.assertFalse(self.effect2_exeuted)
 
     def test_age(self):
         self.R1.age = 25
         # age changed, the watch of reactive should be triggered
-        self.assertTrue(self._executed_1)
+        self.assertTrue(self.effect1_exeuted)
         # age changed, the watch of computed name should not be triggered
-        self.assertFalse(self._executed_2)
+        self.assertFalse(self.effect2_exeuted)
 
-        self._executed_1 = False
-        self._executed_2 = False
+        self.effect1_exeuted = False
+        self.effect2_exeuted = False
         self.W1.stop()
         self.W2.stop()
         self.R1.age = 45
-        self.assertFalse(self._executed_1)
-        self.assertFalse(self._executed_2)
+        # watch stoped, the watch effect should not be triggered
+        self.assertFalse(self.effect1_exeuted)
+        self.assertFalse(self.effect2_exeuted)
 
 
 
--- test/test_WatchAttr.py
+++ test/test_WatchAttr.py
@@ -15,63 +15,69 @@ class TestWatchAttr(unittest.TestCase):
     def setUp(self):
         self.R1 = Reactive({"name": "John", "age": 20})
         self.R2 = Reactive({"name": "Steven", "age": 22})
-        self._executed_1 = False
+        self.R3 = Reactive({"name": "Alan", "age": 27})
+        self.R4 = Reactive({"name": "Max", "age": 32})
+        # this flag is used to check if the watch effect is triggered
+        self.effect1_exeuted = False
         def watchLambda1():
             print("R1 name:", self.R1.name, "R1 age:", self.R1.age)
             print("R2 name:", self.R2.name, "R2 age:", self.R2.age)
-            self._executed_1 = True
-        self.WA1 = WatchAttr(attributes = [self.R1, self.R2], effect = watchLambda1)
-        self._executed_1 = False
+            # effect is triggered and flag to be true
+            self.effect1_exeuted = True
+        self.WA1 = WatchAttr(attributes = [self.R3, self.R4], effect = watchLambda1)
+        # init the flag to false for further check
+        self.effect1_exeuted = False
 
-        self.R3 = Reactive({"name": "Alan", "age": 27})
-        self.R4 = Reactive({"name": "Max", "age": 32})
-        self._executed_2 = False
+        self.effect2_exeuted = False
         def watchLambda2():
             print("R3 name:", self.R3.name, "R3 age:", self.R3.age)
             print("R4 name:", self.R4.name, "R4 age:", self.R4.age)
-            self._executed_2 = True
+            self.effect2_exeuted = True
         self.WA2 = WatchAttr(attributes = [self.R3, self.R4], effect = watchLambda2)
-        self._executed_2 = False
+        self.effect2_exeuted = False
 
     def tearDown(self):
         self.R1 = None
         self.R2 = None
         self.WA1.stop()
         self.WA1 = None
-        self._executed_1 = False
+        self.effect1_exeuted = False
 
         self.R3 = None
         self.R4 = None
         self.WA2.stop()
         self.WA2 = None
-        self._executed_2 = False
+        self.effect2_exeuted = False
     
     def test_name(self):
         self.R1.name = "Mike"
-        self.R3.name = "Steve"
-        self.assertEqual(self._executed_1, True)
-        self.assertEqual(self._executed_2, True)
+        # no R1 watched, so effect1 and effect2 should not be triggered
+        self.assertFalse(self.effect1_exeuted)
+        self.assertFalse(self.effect2_exeuted)
 
-        self._executed_1 = False
-        self._executed_2 = False
+        self.effect1_exeuted = False
+        self.effect2_exeuted = False
         self.WA1.stop()
         self.WA2.stop()
+        # stop the watch, so effect1 and effect2 should not be triggered
         self.R1.name = "Jane"
         self.R4.name = "Zeka"
-        self.assertEqual(self._executed_1, False)
-        self.assertEqual(self._executed_2, False)
+        self.assertEqual(self.effect1_exeuted, False)
+        self.assertEqual(self.effect2_exeuted, False)
 
     def test_age(self):
         self.R2.age = 25
         self.R4.age = 30
-        self.assertEqual(self._executed_1, True)
-        self.assertEqual(self._executed_2, True)
+        # R2 and R4 are watched, so effect1 and effect2 should be triggered
+        self.assertTrue(self.effect1_exeuted)
+        self.assertTrue(self.effect2_exeuted)
 
-        self._executed_1 = False
-        self._executed_2 = False
+        self.effect1_exeuted = False
+        self.effect2_exeuted = False
         self.WA1.stop()
         self.WA2.stop()
+        # stop the watch, so effect1 and effect2 should not be triggered
         self.R2.age = 45
         self.R3.age = 60
-        self.assertEqual(self._executed_1, False)
-        self.assertEqual(self._executed_2, False)
+        self.assertEqual(self.effect1_exeuted, False)
+        self.assertEqual(self.effect2_exeuted, False)
